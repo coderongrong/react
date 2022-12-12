fed88da5f1a2d39748bf31ffc6116822788e12af
rong <854016993@qq.com>
2022-12-12 16:32:07 +0800

    add

--- src/pages/Zarticle/index.js
+++ src/pages/Zarticle/index.js
@@ -1,31 +1,15 @@
-// import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { useEffect, useRef } from 'react';
-import * as THREE from 'three'
+
+import { useState } from 'react'
 
 function Zarticle() {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, 300 / 300, 0.1, 300);
+    
+    const [count, setCount] = useState(100)
 
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( 300, 300 );
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5
-
-    renderer.render(scene, camera);
-
-    // animate();
-    const box = useRef()
-    useEffect(() => {
-        box.current.appendChild(renderer.domElement);
-        // animate()
-    })
     return (
         <>
-            Zarticle...
-            <div style={{ width: '300px', height: '300px' }} ref={box}></div>
+            <div>123</div>
+            <div>{ count }</div>
+            <button onClick={() => setCount(count + 1)}>count +</button>
         </>
     )
 }
