5e250405c561a5cc901f21b72222e492e252dee3
rong <854016993@qq.com>
2023-03-17 15:35:44 +0800

    add

---
+++ src/pages/Home/children/MkInput/data.js
@@ -0,0 +1,34 @@
+
+const formData = () => {
+
+    const data = [
+        {
+            id: '1',
+            label: 'labelA',
+            name: 'gentA',
+            value: 1
+        },
+        {
+            id: '2',
+            label: 'labelB',
+            name: 'gentB',
+            value: 2
+        },
+        {
+            id: '3',
+            label: 'labelC',
+            name: 'gentC',
+            value: 3
+        },
+        {
+            id: '4',
+            label: 'labelD',
+            name: 'gentD',
+            value: 4
+        }
+
+    ]
+    return data
+}
+
+export default formData
--- src/pages/Home/children/MkInput/index.js
+++ src/pages/Home/children/MkInput/index.js
@@ -1,21 +1,25 @@
 import { Form, Input } from 'antd'
 import { forwardRef, useEffect } from 'react'
+import formData from './data'
 
 const MkInput = (props, ref) => {
+  const data = formData()
   const [form] = Form.useForm()
   useEffect(() => {
-    // console.log(props.stateValue)
-    console.log(ref.current.setFieldsValue(props.stateValue))
+    
+    console.log(data)
+    // console.log(ref.current.setFieldsValue(props.stateValue))
   }, [])
   return (
     <>
       <Form ref={ref} form={form} style={{ maxWidth: 400 }}>
-        <Form.Item label="Field A" name="name">
-          <Input />
-        </Form.Item>
-        <Form.Item label="Field B" name="hobby">
-          <Input />
-        </Form.Item>
+        {data.map((item, index) => {
+          return (
+            <Form.Item label={item.label} name={item.name} key={index}>
+              <Input />
+            </Form.Item>
+          )
+        })}
       </Form>
     </>
   )
