ec25154366f0ecc3f8b2ad8baf68169bcffbfb00
rong <854016993@qq.com>
2023-07-14 17:11:40 +0800

    vue 源码init

---
+++ src/vue2/utils/index.js
@@ -0,0 +1,11 @@
+function isObj (data) {
+    return typeof data == 'object' && data != null
+}
+
+function def(obj, key, value) {
+    Object.defineProperty(obj, key, {
+        enumerable: false,
+        configurable: false,
+        value
+    })
+}
